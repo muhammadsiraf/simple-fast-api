b0316e4c52384aa3ad7accee13cb7485544c85d8
muhammadsiraf <derekzify@gmail.com>
2021-04-18 10:38:40 +0700

    feat: complete router file

--- src/router.py
+++ src/router.py
@@ -1,92 +1,122 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
+import model
+from database import SessionLocal, engine
+
+from handler import (
+    create_kingdom as hand_create_kingdom, 
+    create_king as hand_create_king,
+    create_vizier as hand_create_vizier,
+    create_laymen as hand_create_laymen,
+    
+    get_kingdom as hand_get_kingdom, 
+    get_king as hand_get_king,
+    get_vizier as hand_get_vizier,
+    get_laymen as hand_get_laymen,
+    
+    get_kingdom_list as hand_get_kingdom_list,
+    get_king_list as hand_get_king_list,
+    get_vizier_list as hand_get_vizier_list,
+    get_laymen_list as hand_get_laymen_list
+)
+
+from schema import (
+    CreateKingdom,
+    CreateKing,
+    CreateVizier,
+    CreateLaymen, 
+    
+    GetKingdom,
+    GetKing,
+    GetVizier,
+    GetLaymen,
+    
+    GetKingdomList,
+    GetKingList,
+    GetVizierList,
+    GetLaymenList
+)
+
+model.Base.metadata.create_all(bind=engine)
 
 mainRouter = APIRouter()
 
-@mainRouter.get("/kingdom/", tags=["kingdom"])
-async def get_kingdom_list():
-    return{
-        "message":"this is kingdom list"
-    }
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally: 
+        db.close()
+
+
+@mainRouter.get("/kingdom/", tags=["kingdom"], response_model = GetKingdomList)
+async def get_kingdom_list(skip: int = 0, limit:int = 100, db: Session = Depends(get_db)):
+    response = hand_get_kingdom_list(db = db, skip = skip, limit = limit)
+    return response
     
 
-@mainRouter.get("/kingdom/{id}", tags=["kingdom"])
-async def get_kingdom(id: int):
-    return{
-        "message":"this is kingdom by id"
-    }
+@mainRouter.get("/kingdom/{id}", tags=["kingdom"], response_model = GetKingdom)
+async def get_kingdom(id: int, db: Session = Depends(get_db)):
+    response = hand_get_kingdom(db = db, id = id)
+    return response
     
 
 @mainRouter.post("/kingdom/", tags=["kingdom"])
-async def create_kingdom(name: str,):
-    return{
-        "message":f"created kingdom {name}"
-    }
+async def create_kingdom(req: CreateKingdom, db: Session = Depends(get_db)):
+    request = hand_create_kingdom(db = db, req = req)
+    return request
     
     
-
-
-@mainRouter.get("/king/", tags=["king"])
-async def get_king_list():
-    return{
-        "message":"this is king list"
-    }
+@mainRouter.get("/king/", tags=["king"], response_model = GetKingList)
+async def get_king_list(skip:int = 0, limit:int = 100, db: Session = Depends(get_db)):
+    response = hand_get_king_list(db = db , skip=skip, limit = limit)
+    return response
     
 
-@mainRouter.get("/king/{id}", tags=["king"])
-async def get_king(id: int):
-    return{
-        "message":"this is king by id"
-    }
+@mainRouter.get("/king/{id}", tags=["king"], response_model = GetKing)
+async def get_king(id: int, db: Session = Depends(get_db)):
+    response = hand_get_king(db = db, id = id)
+    return response
     
 
 @mainRouter.post("/king/", tags=["king"])
-async def create_king(name: str):
-    return{
-        "message":f"created king name {name}"
-    }
+async def create_king(req: CreateKing, db: Session = Depends(get_db)):
+    request = hand_create_king(db = db, req = req)
+    return request
     
     
+@mainRouter.get("/vizier/", tags=["vizier"], response_model = GetVizierList)
+async def get_vizier_list(skip:int = 0, limit:int = 100, db: Session = Depends(get_db)):
+    response = hand_get_vizier_list(db = db, skip=skip, limit=limit)
+    return response
+    
+
+@mainRouter.get("/vizier/{id}", tags=["vizier"], response_model = GetVizier)
+async def get_vizier(id: int, db: Session = Depends(get_db)):
+    response = hand_get_vizier(db = db, id = id)
+    return response
     
 
 @mainRouter.get("/vizier/", tags=["vizier"])
-async def get_vizier_list():
-    return{
-        "message":"this is vizier list"
-    }
+async def create_vizier(req: CreateVizier, db: Session = Depends(get_db)):
+    request = hand_create_vizier(db = db, req = req)
+    return request
+    
+    
+@mainRouter.get("/laymen/", tags=["laymen"], response_model = GetLaymenList)
+async def get_laymen_list(skip:int = 0, limit:int = 100, db: Session = Depends(get_db)):
+    response = hand_get_laymen_list(db = db, skip=skip, limit=limit)
+    return response
     
 
-@mainRouter.get("/vizier/{id}", tags=["vizier"])
-async def get_vizier(id: int):
-    return{
-        "message":"this is vizier by id"
-    }
-    
-
-@mainRouter.get("/vizier/", tags=["vizier"])
-async def create_vizier(name: str):
-    return{
-        "message":f"created vizier name {name}"
-    }
-    
+@mainRouter.get("/laymen/{id}", tags=["laymen"], response_model = GetLaymen)
+async def get_laymen(id: int, db: Session = Depends(get_db)):
+    response = hand_get_laymen(db = db, id = id)
+    return response
     
 
 @mainRouter.get("/laymen/", tags=["laymen"])
-async def get_laymen_list():
-    return{
-        "message":"this is laymen list"
-    }
-    
-
-@mainRouter.get("/laymen/{id}", tags=["laymen"])
-async def get_laymen(id: int):
-    return{
-        "message":"this is laymen by id"
-    }
-    
-
-@mainRouter.get("/laymen/", tags=["laymen"])
-async def create_laymen(name: str):
-    return{
-        "message":f"created laymen name {name}"
-    }
+async def create_laymen(req: CreateLaymen, db: Session = Depends(get_db)):
+    request = hand_create_laymen(db = db, req = req)
+    return request
